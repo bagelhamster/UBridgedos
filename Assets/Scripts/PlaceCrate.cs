c46549bc3f659ee2aefad57b87515c1e0c7284f8
bagelhamster <82683493+bagelhamster@users.noreply.github.com>
2022-03-31 22:48:14 +0100

    Updated Game

--- Assets/Scripts/PlaceCrate.cs
+++ Assets/Scripts/PlaceCrate.cs
@@ -7,12 +7,20 @@ public class PlaceCrate : MonoBehaviour
     public GameObject Crate1;
     public GameObject Crate2;
     public GameObject Crate3;
+    public GameObject Crate1Ghost;
+    public GameObject Crate2Ghost;
+    public GameObject Crate3Ghost;
     const float rateOfFire = 1f;
     const float speed = 500f;
     float fireDelay;
 
 
-
+    void Start()
+    {
+        Crate1Ghost.gameObject.SetActive(false);
+        Crate2Ghost.gameObject.SetActive(false);
+        Crate3Ghost.gameObject.SetActive(false);
+    }
 
     // Update is called once per frame
     void Update()
@@ -40,6 +48,25 @@ public class PlaceCrate : MonoBehaviour
             shotInstance.GetComponent<Rigidbody2D>().AddForce(transform.forward * speed);
             ScoreScript.scoreValue += 600;
 
+        }
+
+        if (Input.GetButton("Ghost1"))
+        {
+            Crate1Ghost.gameObject.SetActive(true);
+            Crate2Ghost.gameObject.SetActive(false);
+            Crate3Ghost.gameObject.SetActive(false);
+        }
+        if (Input.GetButton("Ghost2"))
+        {
+            Crate1Ghost.gameObject.SetActive(false);
+            Crate2Ghost.gameObject.SetActive(true);
+            Crate3Ghost.gameObject.SetActive(false);
+        }
+        if (Input.GetButton("Ghost3"))
+        {
+            Crate1Ghost.gameObject.SetActive(false);
+            Crate2Ghost.gameObject.SetActive(false);
+            Crate3Ghost.gameObject.SetActive(true);
         }
     }
 }
